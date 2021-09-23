ac9a68b314d7d05bdd3c70f815a5044378b562fc
dima <test@sd.com>
2021-09-23 21:16:25 +0500

    0. Что такое DI? [#6876]

---
+++ WhatIsDI/src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+
+    private Store store;
+    private ConsoleInput consoleInput;
+
+    public StartUI(Store store) {
+        this.store = store;
+    }
+
+    public void add(String value) {
+        store.add(value);
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
